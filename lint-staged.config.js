02ba926304a009ff46992583d0c0a42491aab34d
Mirco Sanguineti <19243840+msanguineti@users.noreply.github.com>
2020-02-27 09:26:59 +0100

    chore: fix extension list (#52)

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,5 +1,5 @@
 module.exports = {
   // '**/*.ts': () => 'tsc -p tsconfig.json --noEmit',
   '**/*.ts': 'eslint --fix',
-  '**/*.{ts,js,json,md.yml}': 'prettier --write'
+  '**/*.{ts,js,json,md,yml}': 'prettier --write'
 }
