985f131c19693865d2e8c37158762958ab3fd3c5
Mirco Sanguineti <19243840+msanguineti@users.noreply.github.com>
2020-10-27 19:59:56 +0200

    fix: return exitCode set by node or 1 if undefined

--- src/utils.ts
+++ src/utils.ts
@@ -2,16 +2,19 @@ import debug from 'debug'
 import * as os from 'os'
 import { HandlerFunction } from './HandlerFunction'
 import { SignalsEvents } from './SignalsEvents'
-
 export const logger = debug('shutdown-cleanup')
+
 export const signals: Set<SignalsEvents> = new Set([
   'SIGTERM',
   'SIGHUP',
   'SIGINT',
   'exit',
 ])
+
 export const handlers: HandlerFunction[] = []
+
 let shuttingDown = false
+
 export const shutdown = async (
   signal: SignalsEvents | Error
 ): Promise<void> => {
@@ -33,11 +36,14 @@ export const shutdown = async (
       ? (signal as NodeJS.ErrnoException).errno
       : os.constants.signals[signal as NodeJS.Signals]
 
-  // should not exit with 0
-  process.exit(!exitCode || exitCode === 0 ? 1 : exitCode)
+  logger('Shutdown exitCode:', exitCode)
+
+  process.exit(exitCode ?? 1)
 }
+
 export const attachListenerForEvent = (event: SignalsEvents): NodeJS.Process =>
   process
     .removeAllListeners(event)
     .addListener(event as NodeJS.Signals, shutdown)
+
 signals.forEach(attachListenerForEvent)
