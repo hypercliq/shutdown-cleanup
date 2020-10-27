64e29bf2d2c979496f4f0c35d610db8e5020a8b2
Mirco Sanguineti <19243840+msanguineti@users.noreply.github.com>
2020-10-27 20:04:18 +0200

    docs: comments and updated README with new exit code section

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,6 @@
 import { HandlerFunction } from './HandlerFunction'
-import {
-  handlers,
-  logger,
-  signals,
-  attachListenerForEvent,
-  shutdown,
-} from './utils'
 import { SignalsEvents } from './SignalsEvents'
+import * as utils from './utils'
 
 /**
  * Allows to register handlers for certain shutdown signals/events
@@ -33,12 +27,12 @@ export class ShutdownCleanup {
    * Register a handler function to run at shutdown.
    *
    * @static
-   * @param {Function} handler
+   * @param {HandlerFunction} handler
    * @memberof ShutdownCleanup
    */
   static registerHandler(handler: HandlerFunction): void {
-    handlers.push(handler)
-    logger('Handler:', handler.toString())
+    utils.handlers.push(handler)
+    utils.logger('Handler:', handler.toString())
   }
 
   /**
@@ -50,10 +44,10 @@ export class ShutdownCleanup {
    * @memberof ShutdownCleanup
    */
   static addSignal(signal: SignalsEvents): boolean {
-    if (signals.has(signal)) return false
-    signals.add(signal)
-    attachListenerForEvent(signal)
-    logger('Added signal:', signal)
+    if (utils.signals.has(signal)) return false
+    utils.signals.add(signal)
+    utils.attachListenerForEvent(signal)
+    utils.logger('Added signal:', signal)
     return true
   }
 
@@ -66,9 +60,9 @@ export class ShutdownCleanup {
    * @memberof ShutdownCleanup
    */
   static removeSignal(signal: SignalsEvents): boolean {
-    if (signals.delete(signal)) {
-      process.removeListener(signal, shutdown)
-      logger('Removed signal:', signal)
+    if (utils.signals.delete(signal)) {
+      process.removeListener(signal, utils.shutdown)
+      utils.logger('Removed signal:', signal)
       return true
     }
     return false
@@ -82,6 +76,6 @@ export class ShutdownCleanup {
    * @memberof ShutdownCleanup
    */
   static listSignals(): SignalsEvents[] {
-    return Array.from(signals)
+    return Array.from(utils.signals)
   }
 }
